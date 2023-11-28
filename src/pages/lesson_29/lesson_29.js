30cf72dcd8706cfaa55ce4eb7097e7d34536675b
IrenaDev <19iryna1992@gmail.com>
2023-11-28 21:13:17 +0100

    lesson-29 start

--- src/pages/lesson_29/lesson_29.js
+++ src/pages/lesson_29/lesson_29.js
@@ -1,1 +1,14 @@
+import * as bootstrap from "bootstrap";
 import "./lesson_29.scss";
+
+const sendNameBtn = document.getElementById("sendName");
+
+sendNameBtn.addEventListener("click", () => {
+  const name = document.getElementById("floatingInputName").value;
+
+  if (name) {
+  } else {
+      
+  }
+  console.log(name);
+});
